cf1743c29f6e302036c25185b31421ed5f9f8163
Dhaerya Jain <dhaeryajain44@gmail.com>
2026-06-28 11:48:45 +0530

    Add files via upload

---
+++ Arrays/Arrays75.java
@@ -0,0 +1,18 @@
+public class Arrays75
+{
+    public void sortColors(int[] nums)
+    {
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    int temp= nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+}
